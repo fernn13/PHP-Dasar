2ee34bae3f0a87bcf9ac5f66565ddb1f3a08e54a
fernn13_ <fernandaadityo13@gmail.com>
2024-05-29 21:14:16 +0700

    first commit

---
+++ 1_Latihan1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Div dan Span</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Belajar PHP">
+    <meta name="keywords" content="233307012">
+    <meta name="author" content="fernanda">
+</head>
+
+<body>
+    <h1>Halaman PHP saya</h1>
+    <?php
+    echo "Hallo ini halaman dengan menggunakan bahasa PHP!";
+    ?>
+</body>
+
+</html>
